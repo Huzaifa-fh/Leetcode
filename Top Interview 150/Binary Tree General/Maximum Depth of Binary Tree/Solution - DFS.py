87930be6174f5e80c4f087a6051d632509c9a6ba
Huzaifa-fh <huzaifabinfaheem1@gmail.com>
2023-07-03 04:29:01 +0500

    Maximum Depth of Binary Tree

---
+++ Top Interview 150/Binary Tree General/Maximum Depth of Binary Tree/Solution - DFS.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        if not root: return 0
+
+        stack = deque()
+        max_depth = 0
+
+        stack.append((root, 1))
+
+        while stack:
+            node, depth = stack.pop()
+
+            max_depth = max(max_depth, depth)
+
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+
+        return max_depth
