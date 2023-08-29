06fd612ca937cae8eecbb21e30060bf535eef347
Huzaifa-fh <huzaifabinfaheem1@gmail.com>
2023-08-29 15:19:16 +0500

    Rotate Array - Bug Fix

--- Top Interview 150/Array - String/Rotate Array/Solution - Simplifed.py
+++ Top Interview 150/Array - String/Rotate Array/Solution - Simplifed.py
@@ -4,5 +4,5 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         k = k % len(nums)
-        nums[::] = nums[-k:] + nums[:-k]
+        nums[:] = nums[-k:] + nums[:-k]
         return nums
